5055e90bb07fb51e5dfb996658d0cc49bd44f873
Roopa-Karanam <roopa.karanam@mohawkcollege.ca>
2022-07-09 15:50:09 -0400

    all tests passed

--- banking.py
+++ banking.py
@@ -1,59 +1,52 @@
+"""
+Represents an Account
+
+"""
 import time
 import datetime
 class Account:
-    """
-    Represents an Account
-    
-    transactions	A list of Transaction instances
-get_balance()	A method which returns the account balance
-deposit(amount)	A method which creates a deposit transaction
-withdraw(amount)	A method which creates a withdrawal transaction
-    """
-    transactions=[]
+    '''
+    Account class'''
+ 
     def __init__(self):
+        '''init method'''
         self.transactions = []
-    
-  
-    def deposit(self,amount):        
-        y=abs(amount)        
-        x=Transaction(y)
-        self.transactions.append(x)
+    def deposit(self,amount):
+        '''deposit method'''
+        y_amount=abs(amount)
+        x_amount=Transaction(y_amount)
+        self.transactions.append(x_amount)
        # return self.transactions+=x
     def withdraw(self, amount):
-        y= abs(amount)
-        y=y*-1
-        x=Transaction(y)
-        self.transactions.append(x)
+        '''withdraw method'''
+        y_amount= abs(amount)
+        y_amount=y_amount*-1
+        x_amount=Transaction(y_amount)
+        self.transactions.append(x_amount)
         #return self.transactions+=x
     def get_balance(self):
+        '''get balance method'''
         bal=0
         for i in range (0,len(self.transactions)):
-            bal=+self.transactions[i]
+            bal=+self.transactions[i].amount
         return bal
-        
-        
-    
 class Transaction():
+    '''Transaction class'''
     def __init__(self, amount, timestamp=None):
         """
-        Extends the Person __init__ and requires an employee_id.
+       init method
         """
         self.amount = amount
-        self.timestamp=datetime.datetime.now()
-        
-
-
+        if timestamp is None:
+            self.timestamp=datetime.datetime.now()
+        else:
+            self.timestamp=timestamp
     def __repr__(self):
-       return 'Transaction(time: ={}, amount={})'.format(
-            repr(self.amount),
-            repr(self.timestamp),
-            
-        )
-    
+        '''repr method'''
+        return 'Transaction(timestamp: ={}, amount={})'.format(
+            (self.timestamp),
+            (self.amount))
     def __str__(self):
-            return 'Account(time: ={}, Balance={})'.format(
-                 repr(self.balance),
-                 repr(self.timestamp),
-                 
-             )
+        '''str method'''
+        return self.timestamp.strftime("%Y-%m-%d") + ": $ "+self.amount
 
--- tests/test_banking.py
+++ tests/test_banking.py
@@ -1,16 +1,50 @@
-from banking import Account
+''' tests for module banking.py'''
+from datetime import datetime
 from banking import Transaction
-import datetime
-datetime.datetime.now()
-
+from banking import Account
 def test_transaaction_amount():
+    '''
+    test for amount
+
+    Returns
+    -------
+    None.
+
+    '''
     transaction = Transaction(100)
     assert transaction.amount == 100
 def test_transaaction_timestamp():
+    '''
+    test for timestamp
+
+    Returns
+    -------
+    None.
+
+    '''
     transaction = Transaction(100)
-    assert transaction.timestamp == datetime.datetime.now()
-'''def test_deposit():
-    account=Account()
-    account.deposit(100)
-    assert  account.get_balance()==100
-'''
+    assert transaction.timestamp == datetime.now()
+def test_deposit():
+    test_account=Account()
+    test_account.deposit(100)
+    test_account.deposit(200)
+    test_account.deposit(300)
+    assert len(test_account.transactions)==3
+def test_withdraw():
+    test_account=Account()
+    test_account.withdraw(100)
+    test_account.withdraw(200)
+    test_account.withdraw(300)
+    assert len(test_account.transactions)==3
+def test_get_balance():
+    test_account=Account()
+    test_account.withdraw(100)
+    test_account.deposit(100)
+    test_account.deposit(200)
+    assert test_account.get_balance()==200
+def test_transactions():
+    test_transactions=Transaction(100, datetime(2023,12,12))
+    assert test_transactions.amount==100
+    assert test_transactions.timestamp==datetime(2023,12,12)
+    
+    
